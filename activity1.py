fe216413cc607b13624926df03bee057033ada4f
Vivienne126 <vivienne.singh@cns.ac.in>
2024-12-14 11:22:09 +0530

    New

---
+++ activity1.py
@@ -0,0 +1,11 @@
+#print a sentense
+print("Hello i am Vivienne")
+
+# print a number
+print(4)
+
+#Next line after print
+print("Welcome \n Vivienne")
+
+# print multiple statements or arguments
+print("Welcome", 4)
