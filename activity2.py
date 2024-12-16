bb8405fe6d77d7df6485af4a023551b311055553
Vivienne126 <vivienne.singh@cns.ac.in>
2024-12-16 21:19:06 +0530

    new

--- activity2.py
+++ activity2.py
@@ -1,9 +1,12 @@
-x=5
-y="Vivienne"
+# Birthdates in Variables
+x="18 August 2014"
 print(x)
+y="18 November 1992"
 print(y)
+a="15 February 2022"
+print(a)
+b="5 Aprill 1995"
+print(b)
+c="15 July 2013"
+print(c)
 
-Vivienne=1
-print(Vivienne)
-number=input ("Enter any number")
-print("The number is",number)
